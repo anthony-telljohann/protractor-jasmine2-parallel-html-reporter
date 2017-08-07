94d89d89e3977ffe1557fc57d5cdb33f97eb6c71
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-07 11:00:02 -0400

    Update _constructor.js

--- test/unit/macros/_constructor.js
+++ test/unit/macros/_constructor.js
@@ -9,12 +9,12 @@ function isInstance(assert, input) {
 isInstance.title = (providedTitle, input, expected) => `${input} should return instance of ProtractorJasmine2ParallelHtmlReporter`
 
 function throwsNonEmptyStringTypeError(assert, input, expected) {
-	assert.is(assert.throws(() => eval(input)), TypeError)).message, REPORTS_DIRECTORY_SHOULD_BE_A_NON_EMPTY_STRING)
+	assert.is(assert.throws(() => eval(input)), TypeError).message, REPORTS_DIRECTORY_SHOULD_BE_A_NON_EMPTY_STRING)
 }
 throwsNonEmptyStringTypeError.title = (providedTitle, input, expected) => `${input} should throw TypeError "${REPORTS_DIRECTORY_SHOULD_BE_A_NON_EMPTY_STRING}"`
 
 function throwsStringTypeError(assert, input, expected) {
-	assert.is(assert.throws(() => eval(input)), TypeError)).message, REPORTS_DIRECTORY_SHOULD_BE_A_STRING)
+	assert.is(assert.throws(() => eval(input)), TypeError).message, REPORTS_DIRECTORY_SHOULD_BE_A_STRING)
 }
 throwsStringTypeError.title = (providedTitle, input, expected) => `${input} should throw TypeError "${REPORTS_DIRECTORY_SHOULD_BE_A_STRING}"`
 
