1a4cc34832a6ff5057186ab2dd3fe2d4d647be3c
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-07 09:47:57 -0400

    Update constructor.js

--- test/unit/constructor.js
+++ test/unit/constructor.js
@@ -6,7 +6,6 @@ const REPORTS_DIRECTORY_SHOULD_BE_A_NON_EMPTY_STRING =
 	'reports directory should be a non-empty string'
 const REPORTS_DIRECTORY_SHOULD_BE_A_STRING =
 	'reports directory should be a string'
-
 const should = {
  return: {
     instance: `should return an instance of ProtractorJasmine2ParallelHtmlReporter`
@@ -18,7 +17,6 @@ const should = {
     }
  }
 }
-
 const test = {
   returns: {
     instance: (value) => (assert) => { assert.true(is.instance(new ProtractorJasmine2ParallelHtmlReporter(value), ProtractorJasmine2ParallelHtmlReporter))}
@@ -30,38 +28,32 @@ const test = {
     }
   }
 }
-
 const suite = spec.describe('constructing')
+
+var scenario
 var currentSuite = suite
 
 scenario = currentSuite.describe('without reports directory')
 scenario(should.return.instance, test.returns.instance())
 
 currentSuite = suite.describe('with reports directory as')
-
 scenario = currentSuite.describe('undefined')
 scenario(should.return.instance, test.returns.instance(undefined))
-
 scenario = currentSuite.describe('non-empty string')
 scenario(should.return.instance, test.returns.instance('reports/'))
 
 scenario = currentSuite.describe('empty string')
 scenario(should.throw.typeError.nonEmptyString, test.throws.typeError.nonEmptyString(' '))
-
 scenario = currentSuite.describe('null')
-scenario(should.throw.typeError.string, test.throws.typeError.string(null))
 
+scenario(should.throw.typeError.string, test.throws.typeError.string(null))
 scenario = currentSuite.describe('true')
 scenario(should.throw.typeError.string, test.throws.typeError.string(true))
-
 scenario = currentSuite.describe('false')
 scenario(should.throw.typeError.string, test.throws.typeError.string(false))
-
-scenario = suite.describe('number')
+scenario = currentSuite.describe('number')
 scenario(should.throw.typeError.string, test.throws.typeError.string(123))
-
-scenario = suite.describe('function')
+scenario = currentSuite.describe('function')
 scenario(should.throw.typeError.string, test.throws.typeError.string(function(){}))
-
 scenario = suite.describe('object')
 scenario(should.throw.typeError.string, test.throws.typeError.string({}))
