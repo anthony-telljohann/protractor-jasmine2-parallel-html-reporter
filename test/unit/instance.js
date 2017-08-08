73c2ebf2306c1a969a296142e9ca0873d982fcc4
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-08 13:11:24 -0400

    Update instance.js

--- test/unit/instance.js
+++ test/unit/instance.js
@@ -3,6 +3,8 @@ import test from 'ava-spec'
 import { hasAddProperty, hasConsolidateReportsProperty, isFunction } from '../assertions/_assertions'
 import ProtractorJasmine2ParallelHtmlReporter from '../../lib/protractor-jasmine2-parallel-html-reporter.js'
 
+console.log('ProtractorJasmine2ParallelHtmlReporter', ProtractorJasmine2ParallelHtmlReporter)
+
 const should = {
   haveProperty: (property) => `should have property "${property}"`,
   beFunction: `should be a function`
