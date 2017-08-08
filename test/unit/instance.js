f610a2e5b38f691f6a6dfdad928490f39492c3f6
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-08 10:05:55 -0400

    Update instance.js

--- test/unit/instance.js
+++ test/unit/instance.js
@@ -2,18 +2,27 @@ import spec from 'ava-spec'
 import is from 'is'
 import ProtractorJasmine2ParallelHtmlReporter from '../../lib/protractor-jasmine2-parallel-html-reporter.js'
 
-var instance
+const assert = {
+  hasProperty: (input, property) => (assert) => { assert.true(input.hasOwnProperty(property) },
+  isFunction: (input) => (assert) => { assert.true(is.fn(input)) }                                                        
+}
 
+var currentSuite
+var subject
+  
+var instance
+  
 spec.beforeEach('construct instance', () => {
   instance = new ProtractorJasmine2ParallelHtmlReporter()
 })
 
-spec.describe('instance', it => {
-  it('should add', assert => {
-    assert.true(is.fn(instance.add))
-  })
+currentSuite = spec.describe('instance')
+subject = currentSuite
+subject('should have add property', assert.hasProperty(instance, 'add'))
+subject('should have consolidateReports property', assert.hasProperty(instance, 'consolidateReports'))
 
-  it('should consolidate reports', assert => {
-    assert.true(is.fn(instance.consolidateReports))
-  })
-})
+subject = currentSuite.describe('add')
+subject('should be a function', assert.isFunction(instance.add))
+
+subject = currentSuite.describe('consolidateReports')
+subject('should be a function', assert.isFunction(instance.consolidateReports))
