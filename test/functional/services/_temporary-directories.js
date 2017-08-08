7c35b8db54e4555cb083f714d303d595fdf364b3
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-08 09:07:09 -0400

    Create _temporary-directories.js

---
+++ test/functional/services/_temporary-directories.js
@@ -0,0 +1,11 @@
+import entriesService from '../../../lib/services/entries'
+import filterReportDirectories from '../../../lib/filters/entries/is-report-directory'
+import temporaryDirectoryService from '../../../lib/services/temporary-directory'
+
+export default {
+  async create() {
+    let temporaryDirectory = temporaryDirectoryService.get()
+    let temporaryDirectoryEntries = await entriesService.getAll(temporaryDirectory)
+    return filterReportDirectories(temporaryDirectoryEntries)
+  }
+}
