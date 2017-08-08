f29ad54d91f437145b070af83c03fe3d77cd08d7
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-08 14:06:19 -0400

    Update instance.js

--- test/unit/instance.js
+++ test/unit/instance.js
@@ -1,32 +1,17 @@
-import test from 'ava-spec'
-
+import spec from 'ava-spec'
 import { hasAddProperty, hasConsolidateReportsProperty, isFunction } from '../assertions/_assertions'
 import ProtractorJasmine2ParallelHtmlReporter from '../../lib/protractor-jasmine2-parallel-html-reporter.js'
 
-console.log('ProtractorJasmine2ParallelHtmlReporter', ProtractorJasmine2ParallelHtmlReporter)
-
-const should = {
-  haveProperty: (property) => `should have property "${property}"`,
-  beFunction: `should be a function`
-}
-
-const INSTANCE = `instance`
-const ADD = `add`
-const CONSOLIDATE_REPORTS = `consolidateReports`
-
 var reporter
-  
+var subject
+
 test.beforeEach(`construct reporter`, () => {
   reporter = new ProtractorJasmine2ParallelHtmlReporter()
-  console.log(reporter)
 })
 
-const instance = test.describe(INSTANCE)
-instance(should.haveProperty(ADD), hasAddProperty(reporter))
-instance(should.haveProperty(CONSOLIDATE_REPORTS), hasConsolidateReportsProperty(reporter))
-
-const addProperty = instance.describe(ADD)
-addProperty(should.beFunction, isFunction(reporter.add))
-
-const consolidateReportsProperty = instance.describe(CONSOLIDATE_REPORTS)
-consolidateReportsProperty(should.beFunction, isFunction(reporter.add))
+const suite = spec.describe('instance')
+subject = suite
+subject('should have property add', hasAddProperty(reporter))
+subject('should consolidate reports', hasConsolidateReportsProperty(reporter))
+subject('and add should be a function', isFunction(reporter.add))
+subject('and consolidateReports should be a function', isFunction(reporter.consolidateReports))
