bfcd33415797775f357b824512bc30cf17f93a8d
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-08 15:30:17 -0400

    Update instance.js

--- test/unit/instance.js
+++ test/unit/instance.js
@@ -1,16 +1,14 @@
-import spec from 'ava-spec'
+import test from 'ava-spec'
 import { hasAddProperty, hasConsolidateReportsProperty, isFunction } from '../assertions/_assertions'
 import ProtractorJasmine2ParallelHtmlReporter from '../../lib/protractor-jasmine2-parallel-html-reporter.js'
 
 var reporter
-var subject
 
 test.beforeEach(`construct reporter`, () => {
   reporter = new ProtractorJasmine2ParallelHtmlReporter()
 })
 
-const suite = spec.describe('instance')
-subject = suite
+const subject = test.describe('instance')
 subject('should have property add', hasAddProperty(reporter))
 subject('should consolidate reports', hasConsolidateReportsProperty(reporter))
 subject('and add should be a function', isFunction(reporter.add))
