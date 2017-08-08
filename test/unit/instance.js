819d21b9065fd5da51d37d606f90615d81ae00c5
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-08 15:32:20 -0400

    Update instance.js

--- test/unit/instance.js
+++ test/unit/instance.js
@@ -9,7 +9,7 @@ test.beforeEach(`construct reporter`, () => {
 })
 
 const subject = test.describe('instance')
-subject('should have property add', hasAddProperty(reporter))
-subject('should consolidate reports', hasConsolidateReportsProperty(reporter))
-subject('and add should be a function', isFunction(reporter.add))
-subject('and consolidateReports should be a function', isFunction(reporter.consolidateReports))
+subject(`should have property add`, hasAddProperty(reporter))
+subject(`should consolidate reports`, hasConsolidateReportsProperty(reporter))
+subject(`and add should be a function`, isFunction(reporter.add))
+subject(`and consolidateReports should be a function`, isFunction(reporter.consolidateReports))
