b7c03d7773b01b8263528501950c4d9f5860a8c1
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-09 16:19:51 -0400

    Update instance.js

--- test/unit/instance.js
+++ test/unit/instance.js
@@ -3,27 +3,31 @@ import is from 'is'
 import { hasAddProperty, hasConsolidateReportsProperty, isFunction } from '../assertions/_assertions'
 import ProtractorJasmine2ParallelHtmlReporter from '../../lib/protractor-jasmine2-parallel-html-reporter.js'
 
-var reporter
-
-test.beforeEach(() => {
-  console.log('before each')
-  reporter = new ProtractorJasmine2ParallelHtmlReporter()
-  reporter.foo = 'this is a property'
-  console.log('reporter', reporter)
+test.beforeEach(t => {
+  t.context.reporter = new ProtractorJasmine2ParallelHtmlReporter()
 })
 
-test(`should have property foo`, assert => {
-  assert.true(is.fn(reporter.hasOwnProperty('foo')))
+test(`functions`, t => {
+  t.true(is.fn(t.context.reporter.add), `add`)
+  t.true(is.fn(t.context.reporter.consolidateReports), `consolidateReports`)
+})
+
+test(t => {
+  t.true(is.fn(t.context.reporter.consolidateReports))
+})
+
+test(`should have property`, t => {
+  t.true(is.fn(reporter.hasOwnProperty('foo')))
 })
 test(`should have property add`, assert => {
-  assert.true(is.fn(reporter.hasOwnProperty('add')))
+  t.true(is.fn(reporter.hasOwnProperty('add')))
 })
 test(`should have property consolidateReports`, assert => {
-  assert.true(is.fn(reporter.hasOwnProperty('consolidateReports')))
+  t.true(is.fn(reporter.hasOwnProperty('consolidateReports')))
 })
 test(`add should be a function`, assert => {
-  assert.true(is.fn(reporter.add))
+  t.true(is.fn(reporter.add))
 })
 test(`consolidateReports should be a function`, assert => {
-  assert.true(is.fn(reporter.consolidateReports))
+  t.true(is.fn(reporter.consolidateReports))
 })
