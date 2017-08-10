18ab24a7fab0ae2a559bbbe40ae9950c218b5fdd
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-10 16:22:23 -0400

    Update index.js

--- test/helpers/fixtures/index.js
+++ test/helpers/fixtures/index.js
@@ -5,39 +5,3 @@ export default {
   temporaryReportsDirectoryFixture,
   temporaryReportsFixture
 }
-
-export const temporaryReports = {
-  create() {
-    console.log('create temporary reports') 
-  },
-  query() {
-    console.log('query') 
-  },
-  read() {
-    console.log('read')
-  },
-  update() {
-    console.log('update')
-  },
-  remove() {
-    console.log('remove')
-  }
-}
-
-export const temporaryReportsDirectories = {
-  create() {
-    console.log('create') 
-  },
-  query() {
-    console.log('query') 
-  },
-  read() {
-    console.log('read')
-  },
-  update() {
-    console.log('update')
-  },
-  remove() {
-    console.log('remove')
-  }
-}
