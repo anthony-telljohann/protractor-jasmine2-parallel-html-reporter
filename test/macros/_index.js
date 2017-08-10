9a06ad335c65428d8bbdd4a971456d34ab512066
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-10 14:46:06 -0400

    Update _index.js

--- test/macros/_index.js
+++ test/macros/_index.js
@@ -1,5 +1,12 @@
 import is from 'is'
 
+const throwsError = constructor => messsage => (t, input) => {
+  const error = t.throws(() => { input() }, constructor)
+  t.is(error.message, messsage)
+}
+const throwsTypeError = throwsError(TypeError)
+
+//generic
 function hasFunction(t, input, functionName) {
   isFunction(t, input[functionName])
 }
@@ -17,3 +24,21 @@ function isInstanceOf(t, input, constructor) {
 }
 isInstanceOf.title = (description, input, constructor) => `${description} is an instance of ${constructor.name}`.trim();
 export isInstanceOf
+
+//lib specific
+function isInstanceOfProtractorJasmine2ParallelHtmlReporter(t, input) {
+  isInstanceOf(t, input, ProtractorJasmine2ParallelHtmlReporter)
+}
+isInstanceOfProtractorJasmine2ParallelHtmlReporter.title = isInstanceOf.title
+export isInstanceOfProtractorJasmine2ParallelHtmlReporter
+
+function throwsReportsDirectoryShouldBeANonEmptyString(t, input) {
+  throwsTypeError(REPORTS_DIRECTORY_SHOULD_BE_A_NON_EMPTY_STRING)(t, input)
+}
+throwsReportsDirectoryShouldBeANonEmptyString.title
+export throwsReportsDirectoryShouldBeANonEmptyString
+
+function throwsReportsDirectoryShouldBeAString(t, input) {
+  throwsTypeError(REPORTS_DIRECTORY_SHOULD_BE_A_STRING)(t, input)
+}
+export throwsReportsDirectoryShouldBeAString
