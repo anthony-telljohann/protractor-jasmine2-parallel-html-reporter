a40dfcb72176aaf2e505b38a7d343484a81a9cae
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-10 14:02:18 -0400

    Update _capabilities-stub.js

--- test/unit/stubs/_capabilities-stub.js
+++ test/unit/stubs/_capabilities-stub.js
@@ -1,5 +1,11 @@
 import sinon from 'sinon'
+import mockCapability from '../mocks/_capability-mock.js'
+
+var get = sinon.stub()
+get.withArgs('platform').returns(mockCapability.platform)
+get.withArgs('browserName').returns(mockCapability.browserName)
+get.withArgs('version').returns(mockCapability.version)
 
 export default {
-  get: sinon.stub()
+  get
 }
