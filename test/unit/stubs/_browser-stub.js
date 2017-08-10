7b7ce92c56b4e010a07ca9702e026f03a8dd1437
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-10 14:03:28 -0400

    Update _browser-stub.js

--- test/unit/stubs/_browser-stub.js
+++ test/unit/stubs/_browser-stub.js
@@ -1,5 +1,9 @@
 import sinon from 'sinon'
+import mockCapabilities from '../mocks/_capabilities-mock.js'
+
+var getCapabilities = sinon.stub()
+getCapabilities.resolves(fakeCapabilities)
 
 export default {
-  getCapabilities: sinon.stub()
+  getCapabilities
 }
