3bcf54ec306120656a48aae1ab2adccce3708cc9
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-10 16:15:27 -0400

    Update index.js

--- test/helpers/stubs/index.js
+++ test/helpers/stubs/index.js
@@ -1,5 +1,4 @@
 import sinon from 'sinon'
-import { capabilityMock } from 'mocks'
 import capabilitiesStub from './capabilities-stub.js'
 import browserStub from './browser-stub.js'
 import jasmineStub from './jasmine-stub.js'
@@ -8,22 +7,4 @@ export {
   browserStub,
   capabilitiesStub,
   jasmineStub
-}
-const capabilitiesStub = {
-  get: sinon.stub()
-}
-capabilitiesStub.get.withArgs('platform').returns(capabilityMock.platform)
-capabilitiesStub.get.withArgs('browserName').returns(capabilityMock.browserName)
-capabilitiesStub.get.withArgs('version').returns(capabilityMock.version)
-
-export const browserStub = {
-  getCapabilities: sinon.stub().resolve(capabilitiesStub)
-}
-
-
-export const browserStub = {
-  getCapabilities: sinon.stub().resolve(capabilitiesStub)
-}
-export const jasmineStub = {
-  
 }
