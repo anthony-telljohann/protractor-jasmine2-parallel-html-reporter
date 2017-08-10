d28886f097d14d86536d04fbc62f2d60dbdeda97
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-10 09:13:15 -0400

    Update reporter.js

--- test/unit/reporter.js
+++ test/unit/reporter.js
@@ -2,32 +2,19 @@ import test from 'ava'
 import is from 'is'
 import ProtractorJasmine2ParallelHtmlReporter from '../../lib/protractor-jasmine2-parallel-html-reporter.js'
 
-test.beforeEach(t => {
-  t.context.reporter = new ProtractorJasmine2ParallelHtmlReporter()
+var reporter
+
+test.beforeEach(() => {
+  reporter = new ProtractorJasmine2ParallelHtmlReporter()
 })
 
-test(`functions`, t => {
-  t.true(is.fn(t.context.reporter.add), `add`)
-  t.true(is.fn(t.context.reporter.consolidateReports), `consolidateReports`)
-})
+test(hasFunction('add'), reporter)
+test(hasFunction('consolidateReports'), reporter)
 
-test(t => {
-  t.true(is.fn(t.context.reporter.consolidateReports))
-})
-
-test(`add should be a function`, t => {
-  t.true(is.fn(t.context.reporter.add))
-})
-test(`consolidateReports should be a function`, t => {
-  t.true(is.fn(t.context.reporter.consolidateReports))
-})
-
-function macro(t, input, expected) {
-  t.is(eval(input), expected)
+function hasFunction(functionName) {
+  return macro
+  function macro(t, input) {
+    
+  }
+  macro.title = (providedTitle, input) => `${providedTitle} has function ${functionName}`.trim();
 }
-
-macro.title = (providedTitle, input, expected) => `${providedTitle} ${input} = ${expected}`.trim()
-
-test(macro, '2 + 2', 4)
-test(macro, '2 * 3', 6)
-test('providedTitle', macro, '3 * 3', 9)
