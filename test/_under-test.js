c2a562296c07de92363e3e2ff8f0ce0eaa4ee71f
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-10 14:47:40 -0400

    Delete _under-test.js

--- test/_under-test.js
+++
@@ -1,6 +0,0 @@
-import ProtractorJasmine2ParallelHtmlReporter from '../lib/protractor-jasmine2-parallel-html-reporter.js'
-
-export const constructor = ProtractorJasmine2ParallelHtmlReporter
-export const reporter = new ProtractorJasmine2ParallelHtmlReporter()
-export const add = reporter.add
-export const consolidateReports = reporter.consolidateReports
