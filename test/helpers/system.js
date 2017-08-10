d5ae96538d83700f4e5c257d4dc6375fa6921ed5
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-10 15:22:15 -0400

    Update system.js

--- test/helpers/system.js
+++ test/helpers/system.js
@@ -1,1 +1,35 @@
+export const temporaryReports = {
+  create() {
+    console.log('create temporary reports') 
+  },
+  query() {
+    console.log('query') 
+  },
+  read() {
+    console.log('read')
+  },
+  update() {
+    console.log('update')
+  },
+  remove() {
+    console.log('remove')
+  }
+}
 
+export const temporaryReportsDirectories = {
+  create() {
+    console.log('create') 
+  },
+  query() {
+    console.log('query') 
+  },
+  read() {
+    console.log('read')
+  },
+  update() {
+    console.log('update')
+  },
+  remove() {
+    console.log('remove')
+  }
+}
