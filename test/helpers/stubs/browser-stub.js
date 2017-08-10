4209985f02734dabf0639327c3bebfec7faacadc
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-10 16:10:22 -0400

    Update browser-stub.js

--- test/helpers/stubs/browser-stub.js
+++ test/helpers/stubs/browser-stub.js
@@ -1,1 +1,9 @@
+import sinon from 'sinon'
+import capabilitiesStub from './capabilities-stub.js'
 
+const browserStub = {
+  getCapabilities: sinon.stub()
+}
+browserStub.getCapabilities.resolve(capabilitiesStub)
+
+export default browserStub
