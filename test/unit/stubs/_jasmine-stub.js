422cf20d0a86018a95d503d64a99774131c1160a
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-10 14:05:16 -0400

    Update _jasmine-stub.js

--- test/unit/stubs/_jasmine-stub.js
+++ test/unit/stubs/_jasmine-stub.js
@@ -1,5 +1,9 @@
 import sinon from 'sinon'
+import jasmineEnvStub from './_jasmine-env-stub.js'
+
+var getEnv = sinon.stub()
+getEnv.withArgs().returns(jasmineEnvStub)
 
 export default {
-  getEnv: sinon.stub()
+  getEnv
 }
