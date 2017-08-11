98c916be68aa42d0b416b2f8271743b4bd4cf25c
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-10 22:37:41 -0400

    Delete _jasmine-stub.js

--- test/unit/stubs/_jasmine-stub.js
+++
@@ -1,9 +0,0 @@
-import sinon from 'sinon'
-import jasmineEnvSpy from '../spies/_jasmine-env-spy.js'
-
-var getEnv = sinon.stub()
-getEnv.withArgs().returns(jasmineEnvSpy)
-
-export default {
-  getEnv
-}
