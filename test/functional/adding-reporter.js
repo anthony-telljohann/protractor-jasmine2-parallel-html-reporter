4b8686a7e1812cfab57f51a718546351011ea709
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-10 22:28:59 -0400

    Update adding-reporter.js

--- test/functional/adding-reporter.js
+++ test/functional/adding-reporter.js
@@ -1,6 +1,6 @@
 import test from 'ava'
-import ProtractorJasmine2ParallelHtmlReporter from 'lib/protractor-jasmine2-parallel-html-reporter.js'
-import { hasFunction } from 'macros/index.js'
+import ProtractorJasmine2ParallelHtmlReporter from '/lib/protractor-jasmine2-parallel-html-reporter.js'
+import { hasFunction } from '/test/helpers/macros/index.js'
 
 const reporter = new ProtractorJasmine2ParallelHtmlReporter();
 
